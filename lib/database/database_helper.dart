27379763af81bd08ec7910746efda0a85dd33a89
om1cael <188113439+om1cael@users.noreply.github.com>
2025-06-28 16:32:21 -0300

    feat(database): add custom_cups table to database initialization

--- lib/database/database_helper.dart
+++ lib/database/database_helper.dart
@@ -19,9 +19,12 @@ class DatabaseHelper {
     String path = join(await getDatabasesPath(), 'hidroly.db');
     return await openDatabase(
       path,
-      onCreate: (db, version) {
-        return db.execute(
+      onCreate: (db, version) async {
+        await db.execute(
           'CREATE TABLE users(id INTEGER PRIMARY KEY, dailyGoal INTEGER, currentAmount INTEGER)'
+        );
+        await db.execute(
+          'CREATE TABLE custom_cups(id INTEGER PRIMARY KEY AUTOINCREMENT, amount INTEGER NOT NULL)'
         );
       },
       version: 1,
--- lib/model/water_button.dart
+++ lib/model/water_button.dart
@@ -1,9 +1,20 @@
 class WaterButton {
-  final int amount;
+  final int? id;
+  final int   amount;
   final bool isCustomOption;
 
   const WaterButton({
+    this.id,
     required this.amount,
     this.isCustomOption = false,
   });
+
+  Map<String, Object?> toMap() {
+    final map = {'amount': amount};
+    if(id != null) {
+      map['id'] = id as int;
+    };
+    
+    return map;
+  }
 }
