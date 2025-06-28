78b9162a5f56a710607551c2dc77ce690cc07921
om1cael <188113439+om1cael@users.noreply.github.com>
2025-06-28 14:26:29 -0300

    feat(setup_controller): implement onSubmit method to calculate daily goal and save user data

--- lib/controller/setup_controller.dart
+++ lib/controller/setup_controller.dart
@@ -1,14 +1,20 @@
 import 'package:flutter/widgets.dart';
+import 'package:hidroly/database/database_helper.dart';
+import 'package:hidroly/model/User.dart';
+import 'package:hidroly/utils/calculate_dailygoal.dart';
 
 class SetupController {
   final TextEditingController ageController = TextEditingController();
   final TextEditingController weightController = TextEditingController();
   final GlobalKey<FormState> formKey = GlobalKey<FormState>();
 
-  void onSubmit() {
+  void onSubmit() async {
     if(formKey.currentState!.validate()) {
-      // save things
-      print('Age: ${ageController.text}, Weight: ${weightController.text}');
+      int age = int.parse(ageController.text);
+      int weight = int.parse(weightController.text);
+
+      int dailyGoal = CalculateDailyGoal().calculate(age, weight);
+      await DatabaseHelper.instance.createUser(User(id: 1, dailyGoal: dailyGoal, currentAmount: 0));
     }
   }
 
