7c8196a42f58720439a6d869b3fe6895d63db488
om1cael <188113439+om1cael@users.noreply.github.com>
2025-06-28 10:56:28 -0300

    feat: create setup page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hidroly/pages/home_page.dart';
+import 'package:hidroly/pages/setup_page.dart';
 
 void main() {
   runApp(const MainApp());
@@ -14,7 +15,7 @@ class MainApp extends StatelessWidget {
       theme: ThemeData(
         fontFamily: 'Poppins',
       ),
-      home: HomePage(),
+      home: SetupPage(),
     );
   }
 }
---
+++ lib/pages/setup_page.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class SetupPage extends StatefulWidget {
+  const SetupPage({super.key});
+
+  @override
+  State<SetupPage> createState() => _SetupPageState();
+}
+
+class _SetupPageState extends State<SetupPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(0xff242530),
+      ),
+      backgroundColor: Color(0xff242530),
+      body: Center(
+        child: Column(
+          children: [],
+        ),
+      ),
+    );
+  }
+}
