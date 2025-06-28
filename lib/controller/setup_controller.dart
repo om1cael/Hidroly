a7b874b950cb2cb752d3838a2c1c5dd710e8ddb1
om1cael <188113439+om1cael@users.noreply.github.com>
2025-06-28 12:32:48 -0300

    feat(setup_page): add validation logic for age and weight inputs

--- lib/controller/setup_controller.dart
+++ lib/controller/setup_controller.dart
@@ -3,4 +3,17 @@ import 'package:flutter/widgets.dart';
 class SetupController {
   final TextEditingController ageController = TextEditingController();
   final TextEditingController weightController = TextEditingController();
+
+  bool validate() {
+    int ageValue = int.parse(ageController.text);
+    int weightValue = int.parse(weightController.text);
+
+    if(ageValue < 10 || weightValue > 200) return false;
+    return true;
+  }
+
+  void dispose() {
+    ageController.dispose();
+    weightController.dispose();
+  }
 }
