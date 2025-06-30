a2e0eb1854b1665e93593d245dd67b5af3b0bc20
om1cael <188113439+om1cael@users.noreply.github.com>
2025-06-30 20:18:26 -0300

    style: add space for consistency in HomePage widget declaration

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: AppTheme.darkTheme,
-      home:const HomePage(),
+      home: const HomePage(),
     );
   }
 }
