f7dff11a7cbc9386c07fc7f2e4e6604c61b89283
om1cael <188113439+om1cael@users.noreply.github.com>
2025-06-30 19:35:41 -0300

    refactor(user_provider): remove userStatus management from UserProvider

--- lib/provider/user_provider.dart
+++ lib/provider/user_provider.dart
@@ -6,18 +6,8 @@ class UserProvider extends ChangeNotifier {
   User? _user;
   User? get user => _user;
 
-  UserStatus _userStatus = UserStatus.loading;
-  UserStatus get userStatus => _userStatus;
-
   Future<void> loadUser() async {
     _user = await DatabaseHelper.instance.getUser(1);
-
-    if(user == null) {
-      _userStatus = UserStatus.empty;
-    } else {
-      _userStatus = UserStatus.loaded;
-    }
-
     notifyListeners();
   }
   
@@ -35,5 +25,3 @@ class UserProvider extends ChangeNotifier {
     notifyListeners();
   }
 }
-
-enum UserStatus {loading, loaded, empty}
