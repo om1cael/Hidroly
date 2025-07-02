e0464f70f265eea7d2700c624ac9dc5c91f69de2
om1cael <188113439+om1cael@users.noreply.github.com>
2025-07-02 16:50:12 -0300

    refactor: replace DatabaseHelper with UserLocalDataSourceImpl in SetupController

--- lib/controller/setup_controller.dart
+++ lib/controller/setup_controller.dart
@@ -1,8 +1,12 @@
-import 'package:hidroly/data/database/database_helper.dart';
+import 'package:hidroly/data/datasource/user_local_datasource_impl.dart';
 import 'package:hidroly/model/user.dart';
 import 'package:hidroly/utils/calculate_dailygoal.dart';
 
 class SetupController {
+  final UserLocalDataSourceImpl _userLocalDataSourceImpl;
+
+  SetupController(this._userLocalDataSourceImpl);
+
   Future<bool> save(String ageText, String weightText) async {
     int? age = int.tryParse(ageText);
     int? weight = int.tryParse(weightText);
@@ -10,7 +14,7 @@ class SetupController {
     if(age == null || weight == null) return false;
 
     int dailyGoal = CalculateDailyGoal().calculate(age, weight);
-    await DatabaseHelper.instance.createUser(User(id: 1, dailyGoal: dailyGoal, currentAmount: 0));
+    await _userLocalDataSourceImpl.createUser(User(id: 1, dailyGoal: dailyGoal, currentAmount: 0));
     return true;
   }
 }
