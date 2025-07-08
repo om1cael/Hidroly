f8d78e3a33351d59c692fd1294223980186a3910
om1cael <188113439+om1cael@users.noreply.github.com>
2025-07-08 14:01:42 -0300

    feat(home_page): add daily history modal to display water consumption records

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:hidroly/data/model/history_entry.dart';
 import 'package:hidroly/pages/setup_page.dart';
 import 'package:hidroly/provider/custom_cups_provider.dart';
+import 'package:hidroly/provider/daily_history_provider.dart';
 import 'package:hidroly/provider/day_provider.dart';
 import 'package:hidroly/theme/app_colors.dart';
 import 'package:hidroly/widgets/home/home_bottom_nav.dart';
 import 'package:hidroly/widgets/home/water_action_buttons.dart';
 import 'package:hidroly/widgets/home/water_progress_circle.dart';
+import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 
 class HomePage extends StatefulWidget {
@@ -71,6 +74,93 @@ class _HomePageState extends State<HomePage> {
       ),
       actions: [
         IconButton(
+          onPressed: () {
+            context.read<DailyHistoryProvider>().getAll(1);
+            List<HistoryEntry> historyList = context.read<DailyHistoryProvider>().history;
+
+            showModalBottomSheet(
+              context: context, 
+              builder: (builder) {
+                return DraggableScrollableSheet(
+                  expand: false,
+                  builder: (context, scrollController) { 
+                    return Padding(
+                      padding: const EdgeInsets.all(15.0),
+                      child: Column(
+                        children: [
+                          Row(
+                            children: [
+                              IconButton(
+                                onPressed: () => Navigator.of(context).pop(), 
+                                icon: Icon(
+                                  Icons.arrow_back,
+                                  color: AppColors.primaryText,
+                                ),
+                                style: IconButton.styleFrom(
+                                  backgroundColor: Colors.transparent,
+                                ),
+                              ),
+                              Text(
+                                'Today\'s history',
+                                style: TextStyle(
+                                  color: AppColors.primaryText
+                                ),
+                              )
+                            ],
+                          ),
+                          Expanded(
+                            child: ListView.builder(
+                              controller: scrollController,
+                              shrinkWrap: true,
+                              itemBuilder: (context, index) {
+                                final history = historyList[index];
+                                return Card(
+                                  color: AppColors.onBackground,
+                                  child: ListTile(
+                                    leading: Icon(
+                                      Icons.local_drink,
+                                      color: AppColors.blueAccent,
+                                    ),
+                                    title: Text(
+                                      '${history.amount}ml',
+                                      style: Theme.of(context).textTheme.bodyLarge
+                                    ),
+                                    subtitle: Text(
+                                      'Added ${DateFormat.Hm().format(history.dateTime.toLocal())}',
+                                      style: Theme.of(context).textTheme.bodySmall
+                                    ),
+                                    trailing: IconButton(
+                                      onPressed: () {
+                                        // TODO: Remove water from currentAmount
+                                      },
+                                      icon: Icon(
+                                        Icons.delete_forever,
+                                        color: Colors.redAccent,
+                                      ),
+                                    ),
+                                  ),
+                                );
+                              }, 
+                              itemCount: historyList.length
+                            ),
+                          ),
+                        ]
+                      ),
+                    );
+                  }
+                );
+              }
+            );
+          }, 
+          icon: Icon(
+            Icons.list, 
+            color: AppColors.primaryText,
+          ),
+          style: IconButton.styleFrom(
+            backgroundColor: Colors.transparent,
+          ),
+        ),
+        IconButton(
           onPressed: () {}, 
           icon: Icon(
             Icons.settings, 
@@ -79,7 +169,7 @@ class _HomePageState extends State<HomePage> {
           style: IconButton.styleFrom(
             backgroundColor: Colors.transparent,
           ),
-        )
+        ),
       ],
     );
   }
