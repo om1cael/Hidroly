d1dfff834e45018e834a8faf5f2c023cd6250dd4
om1cael <188113439+om1cael@users.noreply.github.com>
2025-07-08 11:47:49 -0300

    feat: add daily history repository

---
+++ lib/data/repository/daily_history_repository.dart
@@ -0,0 +1,17 @@
+import 'package:hidroly/data/datasource/daily_history_local_datasource_impl.dart';
+import 'package:hidroly/data/model/history_entry.dart';
+
+class DailyHistoryRepository {
+  final DailyHistoryLocalDataSourceImpl _dataSource;
+
+  const DailyHistoryRepository(this._dataSource);
+
+  Future<void> create(HistoryEntry historyEntry) async => 
+    _dataSource.create(historyEntry);
+  
+  Future<List<HistoryEntry>?> getAll(int day) async =>
+    _dataSource.getAll(day);
+
+  Future<void> delete(int id) async =>
+    _dataSource.delete(id);
+}
