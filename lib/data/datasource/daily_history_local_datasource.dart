d8278dfb59613d955243bc319dbeaf598157fcc2
om1cael <188113439+om1cael@users.noreply.github.com>
2025-07-08 11:45:00 -0300

    feat: implement DailyHistoryLocalDataSourceImpl CRUD

--- lib/data/datasource/daily_history_local_datasource.dart
+++ lib/data/datasource/daily_history_local_datasource.dart
@@ -2,6 +2,6 @@ import 'package:hidroly/data/model/history_entry.dart';
 
 abstract class DailyHistoryLocalDataSource {
   Future<void> create(HistoryEntry historyEntry);
-  Future<List<HistoryEntry>> getAll();
+  Future<List<HistoryEntry>> getAll(int day);
   Future<void> delete(int id);
 }
