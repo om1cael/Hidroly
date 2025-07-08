e9a02037adef9ad8b82977e87c68362434cc417a
om1cael <188113439+om1cael@users.noreply.github.com>
2025-07-08 14:41:26 -0300

    feat(home_page): enhance daily history modal with delete functionality for water entries

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -75,77 +75,85 @@ class _HomePageState extends State<HomePage> {
       actions: [
         IconButton(
           onPressed: () {
-            context.read<DailyHistoryProvider>().getAll(1);
-            List<HistoryEntry> historyList = context.read<DailyHistoryProvider>().history;
-
             showModalBottomSheet(
               context: context, 
               builder: (builder) {
                 return DraggableScrollableSheet(
                   expand: false,
                   builder: (context, scrollController) { 
-                    return Padding(
-                      padding: const EdgeInsets.all(15.0),
-                      child: Column(
-                        children: [
-                          Row(
+                    return Consumer<DailyHistoryProvider>(
+                      builder: (context, provider, _) {
+                        final historyList = provider.history;
+
+                        return Padding(
+                          padding: const EdgeInsets.all(15.0),
+                          child: Column(
                             children: [
-                              IconButton(
-                                onPressed: () => Navigator.of(context).pop(), 
-                                icon: Icon(
-                                  Icons.arrow_back,
-                                  color: AppColors.primaryText,
-                                ),
-                                style: IconButton.styleFrom(
-                                  backgroundColor: Colors.transparent,
-                                ),
-                              ),
-                              Text(
-                                'Today\'s history',
-                                style: TextStyle(
-                                  color: AppColors.primaryText
-                                ),
-                              )
-                            ],
-                          ),
-                          Expanded(
-                            child: ListView.builder(
-                              controller: scrollController,
-                              shrinkWrap: true,
-                              itemBuilder: (context, index) {
-                                final history = historyList[index];
-                                return Card(
-                                  color: AppColors.onBackground,
-                                  child: ListTile(
-                                    leading: Icon(
-                                      Icons.local_drink,
-                                      color: AppColors.blueAccent,
+                              Row(
+                                children: [
+                                  IconButton(
+                                    onPressed: () => Navigator.of(context).pop(), 
+                                    icon: Icon(
+                                      Icons.arrow_back,
+                                      color: AppColors.primaryText,
                                     ),
-                                    title: Text(
-                                      '${history.amount}ml',
-                                      style: Theme.of(context).textTheme.bodyLarge
-                                    ),
-                                    subtitle: Text(
-                                      'Added ${DateFormat.Hm().format(history.dateTime.toLocal())}',
-                                      style: Theme.of(context).textTheme.bodySmall
-                                    ),
-                                    trailing: IconButton(
-                                      onPressed: () {
-                                        // TODO: Remove water from currentAmount
-                                      },
-                                      icon: Icon(
-                                        Icons.delete_forever,
-                                        color: Colors.redAccent,
-                                      ),
+                                    style: IconButton.styleFrom(
+                                      backgroundColor: Colors.transparent,
                                     ),
                                   ),
-                                );
-                              }, 
-                              itemCount: historyList.length
-                            ),
+                                  Text(
+                                    'Today\'s history',
+                                    style: TextStyle(
+                                      color: AppColors.primaryText
+                                    ),
+                                  )
+                                ],
+                              ),
+                              Expanded(
+                                child: ListView.builder(
+                                  controller: scrollController,
+                                  shrinkWrap: true,
+                                  itemBuilder: (context, index) {
+                                    final history = historyList[index];
+                                    return Card(
+                                      color: AppColors.onBackground,
+                                      child: ListTile(
+                                        leading: Icon(
+                                          Icons.local_drink,
+                                          color: AppColors.blueAccent,
+                                        ),
+                                        title: Text(
+                                          '${history.amount}ml',
+                                          style: Theme.of(context).textTheme.bodyLarge
+                                        ),
+                                        subtitle: Text(
+                                          'Added ${DateFormat.Hm().format(history.dateTime.toLocal())}',
+                                          style: Theme.of(context).textTheme.bodySmall
+                                        ),
+                                        trailing: IconButton(
+                                          onPressed: () async {
+                                            await context.read<DayProvider>()
+                                              .removeWater(history.amount);
+                                            
+                                            if(!context.mounted) return;
+                                            await context.read<DailyHistoryProvider>()
+                                              .delete(history.id!, 1);
+                                          },
+                                          icon: Icon(
+                                            Icons.delete_forever,
+                                            color: Colors.redAccent,
+                                          ),
+                                        ),
+                                      ),
+                                    );
+                                  }, 
+                                  itemCount: historyList.length
+                                ),
+                              ),
+                            ]
                           ),
-                        ]
-                      ),
+                        );
+                      }
                     );
                   }
                 );
