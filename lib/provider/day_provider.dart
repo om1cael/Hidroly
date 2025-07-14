47d22baaf506a29fe97d2800883f198003886776
om1cael <188113439+om1cael@users.noreply.github.com>
2025-07-14 18:22:43 -0300

    feat(day_provider): implement method to create new day if necessary

--- lib/provider/day_provider.dart
+++ lib/provider/day_provider.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/widgets.dart';
 import 'package:hidroly/data/model/day.dart';
 import 'package:hidroly/data/repository/day_repository.dart';
-import 'package:hidroly/utils/calculate_dailygoal.dart';
 
 class DayProvider extends ChangeNotifier {
   late DayRepository _repository;
@@ -21,6 +20,35 @@ class DayProvider extends ChangeNotifier {
       )
     );
     return true;
+  }
+
+  /// Creates a new day if the current date differs from the last loaded date.
+  /// If all goes well, it will load the new day using [findLatest].
+  ///
+  /// Does nothing if no day is currently loaded.
+  /// Make sure to call [findLatest] before using this method.
+  ///
+  /// Internally calls [create] using the last day's daily goal.
+  Future<void> createAndLoadIfNewDay() async {
+    if(_day == null) return;
+
+    final appDate = DateTime.utc(
+      _day!.date.year,
+      _day!.date.month,
+      _day!.date.day,
+    );
+
+    final now = DateTime.now().toUtc();
+    final currentDate = DateTime.utc(
+      now.year, 
+      now.month, 
+      now.day
+    );
+
+    if (appDate != currentDate) {
+      await create(_day!.dailyGoal);
+      await findLatest();
+    }
   }
 
   Future<void> findLatest() async {
