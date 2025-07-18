a83ff881b81e440dbfbdd383ab71f8397dbe767b
om1cael <188113439+om1cael@users.noreply.github.com>
2025-07-18 12:09:08 -0300

    fix(app_date_utils): ensure local date normalization in normalizedLocal method

--- lib/utils/app_date_utils.dart
+++ lib/utils/app_date_utils.dart
@@ -3,6 +3,8 @@ import 'package:intl/intl.dart';
 class AppDateUtils {
   /// Takes a *local* date and returns [YYYY-MM-DD 00:00:00.000].
   static DateTime normalizedLocal(DateTime localDate) {
+    localDate = localDate.toLocal();
+    
     return DateTime(
       localDate.year,
       localDate.month,
