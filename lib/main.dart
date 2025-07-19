a778b342901c99b3ef3507a428d82c4e3e9977ae
om1cael <188113439+om1cael@users.noreply.github.com>
2025-07-19 12:01:33 -0300

    feat: add localization support for English and Portuguese

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:hidroly/config/providers.dart';
 import 'package:hidroly/pages/home_page.dart';
 import 'package:hidroly/theme/app_theme.dart';
@@ -19,6 +20,15 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: [
+        Locale('en'),
+        Locale('pt', 'BR'),
+      ],
       theme: AppTheme.darkTheme,
       home: const HomePage(),
     );
