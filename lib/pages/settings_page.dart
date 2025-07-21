01890d399f0cf040dcf866b166e53f1a8412de7e
om1cael <188113439+om1cael@users.noreply.github.com>
2025-07-21 18:18:00 -0300

    refactor(settings): move SettingsGeneral and SettingsYou to widgets directory

--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:hidroly/l10n/app_localizations.dart';
-import 'package:hidroly/pages/settings/settings_general.dart';
-import 'package:hidroly/pages/settings/settings_you.dart';
+import 'package:hidroly/widgets/settings/sections/settings_general.dart';
+import 'package:hidroly/widgets/settings/sections/settings_you.dart';
 import 'package:hidroly/provider/settings_provider.dart';
 import 'package:hidroly/theme/app_colors.dart';
 import 'package:provider/provider.dart';
