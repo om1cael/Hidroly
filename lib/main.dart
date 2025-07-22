ae71ff5fe4defa71660b9caa5528ce5d4cae724e
om1cael <188113439+om1cael@users.noreply.github.com>
2025-07-22 15:45:49 -0300

    chore(deps): add workmanager

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,23 @@ import 'package:hidroly/l10n/app_localizations.dart';
 import 'package:hidroly/pages/home_page.dart';
 import 'package:hidroly/theme/app_theme.dart';
 import 'package:provider/provider.dart';
+import 'package:workmanager/workmanager.dart';
+
+@pragma('vm:entry-point')
+void callbackDispatcher() {
+  Workmanager().executeTask((task, inputData) {
+    // TODO: Notification logic
+    return Future.value(true);
+  });
+}
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  Workmanager().initialize(
+    callbackDispatcher,
+    isInDebugMode: true
+  );
+
   runApp(
     MultiProvider(
       providers: Providers().providers,
