aa0628a75d85c7006a76199e1a37b7bf8b881090
om1cael <188113439+om1cael@users.noreply.github.com>
2025-08-03 18:30:07 -0300

    feat(water_action_buttons): add menu to edit and delete on long press

--- lib/widgets/home/water_action_buttons.dart
+++ lib/widgets/home/water_action_buttons.dart
@@ -7,6 +7,7 @@ import 'package:hidroly/provider/daily_history_provider.dart';
 import 'package:hidroly/provider/day_provider.dart';
 import 'package:hidroly/theme/app_colors.dart';
 import 'package:hidroly/utils/unit_tools.dart';
+import 'package:hidroly/widgets/input/number_input_dialog.dart';
 import 'package:provider/provider.dart';
 
 class WaterActionButtons extends StatelessWidget {
@@ -33,20 +34,106 @@ class WaterActionButtons extends StatelessWidget {
         itemBuilder: (context, index) {
           var cup = customCups[index];
 
-          return ActionChip(
-            onPressed: () async {
-              final int amount = cup.amount;
-              await addWater(context, amount);
-          
-              if(!context.mounted) return;
-              await saveWaterToHistory(context, amount);
+          return GestureDetector(
+            onLongPressStart: (details) async {
+              final RenderBox overlay = Overlay.of(context).context.findRenderObject() as RenderBox;
+              final Offset tapPosition = details.globalPosition;
+
+              await showMenu(
+                context: context, 
+                items: <PopupMenuEntry<String>>[
+                  PopupMenuItem(
+                    child: Row(
+                      spacing: 12,
+                      children: [
+                        Icon(
+                          Icons.edit,
+                          color: AppColors.primaryText,
+                        ),
+                        Text(
+                          'Edit',
+                          style: TextStyle(
+                            color: AppColors.secondaryText
+                          )
+                        ),
+                      ],
+                    ),
+                    onTap: () async {
+                      await showDialog(
+                        context: context, 
+                        builder: (context) {
+                          return Form(
+                            // TODO: Use localized messages
+                            child: NumberInputDialog(
+                              title: 'Edit custom cup', 
+                              inputFieldLabel: 'Amount', 
+                              actionButtonText: 'Update', 
+                              cancelButtonText: 'Cancel', 
+                              inputFieldValidator: (value) {
+                                double? amount = double.tryParse(value ?? '');
+                                if(amount == null || amount <= 0) return AppLocalizations.of(context)!.textFieldAmountError;
+                                return null;
+                              },
+                              onActionPressed: () {
+                                // TODO: Update custom cup
+                              },
+                              onCancelPressed: () {
+                                Navigator.of(context).pop();
+                              }, 
+                              textEditingController: TextEditingController(),
+                            ),
+                          );
+                        }
+                      );
+                    },
+                  ),
+                  PopupMenuItem(
+                    child: Row(
+                      spacing: 12,
+                      children: [
+                        Icon(
+                          Icons.delete_forever,
+                          color: Colors.redAccent,
+                        ),
+                        Text(
+                          'Delete',
+                          style: TextStyle(
+                            color: AppColors.secondaryText
+                          ),
+                        ),
+                      ],
+                    ),
+                    onTap: () async {
+                      await context.read<CustomCupsProvider>()
+                        .deleteCustomCup(cup.id!);
+                    },
+                  ),
+                ],
+                color: Theme.of(context).colorScheme.onSurface,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadiusGeometry.circular(12)
+                ),
+                position: RelativeRect.fromRect(
+                  tapPosition & Size(40, 40), // Size of the menu
+                  Offset.zero & overlay.size,
+                ),
+              );
             },
-            avatar: Icon(Icons.water_drop),
-            backgroundColor: Theme.of(context).colorScheme.onSurface,
-            label: Text(
-              UnitTools.getVolumeWithUnit(cup.amount, isMetric, context: context),
-              style: TextStyle(
-                color: AppColors.secondaryText
+            child: ActionChip(
+              onPressed: () async {
+                final int amount = cup.amount;
+                await addWater(context, amount);
+            
+                if(!context.mounted) return;
+                await saveWaterToHistory(context, amount);
+              },
+              avatar: Icon(Icons.water_drop),
+              backgroundColor: Theme.of(context).colorScheme.onSurface,
+              label: Text(
+                UnitTools.getVolumeWithUnit(cup.amount, isMetric, context: context),
+                style: TextStyle(
+                  color: AppColors.secondaryText
+                ),
               ),
             ),
           );
