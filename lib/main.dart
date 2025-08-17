a5566d9114a8855b80509e9e702217584550cdcf
om1cael <188113439+om1cael@users.noreply.github.com>
2025-08-17 12:03:16 -0300

    feat: load stored theme on startup

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:hidroly/config/providers.dart';
 import 'package:hidroly/l10n/app_localizations.dart';
 import 'package:hidroly/pages/home_page.dart';
+import 'package:hidroly/provider/settings_provider.dart';
 import 'package:hidroly/services/notification_service.dart';
 import 'package:hidroly/services/notification_task_service.dart';
 import 'package:hidroly/theme/app_theme.dart';
@@ -39,16 +40,32 @@ void main() async {
   );
 }
 
-class MainApp extends StatelessWidget {
-  static final ValueNotifier<ThemeMode> themeNotifier =
+class MainApp extends StatefulWidget {
+  static ValueNotifier<ThemeMode> themeNotifier =
     ValueNotifier(ThemeMode.system);
   
   const MainApp({super.key});
 
   @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  @override
+  void initState() {
+    super.initState();
+
+    WidgetsBinding.instance.addPostFrameCallback((_) async {
+      ThemeMode? theme = await context.read<SettingsProvider>().readTheme();
+      MainApp.themeNotifier.value = theme!;
+    });
+  }
+
+
+  @override
   Widget build(BuildContext context) {
     return ValueListenableBuilder<ThemeMode>(
-      valueListenable: themeNotifier,
+      valueListenable: MainApp.themeNotifier,
       builder: (_, theme, __) {
         return MaterialApp(
           localizationsDelegates: AppLocalizations.localizationsDelegates,
