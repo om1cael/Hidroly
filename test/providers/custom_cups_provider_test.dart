c40ef46c82f3e7182d47b08d4ecfd22e7d63c389
om1cael <188113439+om1cael@users.noreply.github.com>
2025-09-02 19:34:46 -0300

    test(custom_cups_provider): remove datasource from test

--- test/providers/custom_cups_provider_test.dart
+++ test/providers/custom_cups_provider_test.dart
@@ -1,24 +1,22 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:hidroly/data/datasource/custom_cups_local_datasource_impl.dart';
 import 'package:hidroly/domain/models/water_button.dart';
 import 'package:hidroly/data/repository/custom_cups_repository.dart';
 import 'package:hidroly/provider/custom_cups_provider.dart';
 import 'package:mocktail/mocktail.dart';
 
-class MockCustomCupsLocalDataSourceImpl extends Mock implements CustomCupsLocalDataSourceImpl {}
+class MockCustomCupsRepository extends Mock implements CustomCupsRepository {}
 
 void main() {
   late CustomCupsProvider provider;
   late CustomCupsRepository repository;
-  late MockCustomCupsLocalDataSourceImpl mockDataSource;
+  late MockCustomCupsRepository mockDataSource;
 
   setUpAll(() {
     registerFallbackValue(WaterButton(amount: 1));
   });
 
   setUp(() {
-    mockDataSource = MockCustomCupsLocalDataSourceImpl();
-    repository = CustomCupsRepository(mockDataSource);
+    repository = MockCustomCupsRepository();
     provider = CustomCupsProvider();
 
     provider.setRepository(repository);
@@ -28,30 +26,30 @@ void main() {
     test('Should create if amount is positive', () async {
       final cups = [WaterButton(id: 1, amount: 300)];
 
-      when(() => mockDataSource.createCustomCup(any()))
+      when(() => repository.createCustomCup(any()))
         .thenAnswer((_) async {});
 
-      when(() => mockDataSource.getAllCustomCups())
+      when(() => repository.getAllCustomCups())
         .thenAnswer((_) async => cups);
       
       final result = await provider.createCustomCup(300);
 
       expect(result, true);
       expect(provider.customCups, cups);
-      verify(() => mockDataSource.createCustomCup(any())).called(1);
-      verify(() => mockDataSource.getAllCustomCups()).called(1);
+      verify(() => repository.createCustomCup(any())).called(1);
+      verify(() => repository.getAllCustomCups()).called(1);
     });
 
     test('Should not create if amount is zero', () async {
       expect(await provider.createCustomCup(0), false);
-      verifyNever(() => mockDataSource.createCustomCup(any()));
-      verifyNever(() => mockDataSource.getAllCustomCups());
+      verifyNever(() => repository.createCustomCup(any()));
+      verifyNever(() => repository.getAllCustomCups());
     });
 
     test('Should not create if amount is negative', () async {
       expect(await provider.createCustomCup(-1), false);
-      verifyNever(() => mockDataSource.createCustomCup(any()));
-      verifyNever(() => mockDataSource.getAllCustomCups());
+      verifyNever(() => repository.createCustomCup(any()));
+      verifyNever(() => repository.getAllCustomCups());
     });
   });
 }
