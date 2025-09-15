2f9372f7a00ebffb8c50c70178f953cc305f09fb
om1cael <188113439+om1cael@users.noreply.github.com>
2025-09-15 16:31:35 +0000

    feat: structure base summary screen

--- lib/ui/summary/view/summary_screen.dart
+++ lib/ui/summary/view/summary_screen.dart
@@ -1,10 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:hidroly/l10n/app_localizations.dart';
+import 'package:hidroly/pages/settings_page.dart';
 
 class SummaryScreen extends StatelessWidget {
   const SummaryScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: appBar(context),
+      body: Padding(
+        padding: EdgeInsetsGeometry.all(16),
+        child: SafeArea(
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                // TODO
+              ],
+            ),
+          )
+        ),
+      ),
+    );
+  }
+
+  AppBar appBar(BuildContext context) {
+    return AppBar(
+      title: Text(AppLocalizations.of(context)!.bottomNavSummaryLabel),
+      actions: [
+        IconButton(
+          onPressed: () {
+            Navigator.of(context).push(
+              MaterialPageRoute(builder: (_) => SettingsPage())
+            );
+          }, 
+          icon: Icon(
+            Icons.settings,
+            color: Theme.of(context).iconTheme.color,
+          ),
+        ),
+      ],
+    );
   }
 }
