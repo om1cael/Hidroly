82c63d6b0a92b1e14fe1b77c3c70e18f4e4a962c
om1cael <188113439+om1cael@users.noreply.github.com>
2025-09-15 16:17:34 +0000

    refactor: use home page to store bottom navigation bar data

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:hidroly/l10n/app_localizations.dart';
+import 'package:hidroly/ui/water_tracking/view/water_tracking_screen.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,8 +10,37 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final List<Widget> _screens = [WaterTrackingScreen()];
+  int _currentIndex = 0;
+  
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: IndexedStack(
+        index: _currentIndex,
+        children: _screens,
+      ),
+      bottomNavigationBar: Theme(
+        data: Theme.of(context).copyWith(
+          splashColor: Colors.transparent,
+        ),
+        child: BottomNavigationBar(
+          items: <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: AppLocalizations.of(context)!.bottomNavHomeLabel,
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.bar_chart),
+              label: AppLocalizations.of(context)!.bottomNavSummaryLabel,
+            ),
+          ],
+          onTap: (newIndex) => setState(() {
+            _currentIndex = newIndex;
+          }),
+          currentIndex: _currentIndex,
+        ),
+      ),
+    );
   }
 }
