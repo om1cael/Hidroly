302db2c733eb60a2caceaef19dc794a50562dd6d
om1cael <188113439+om1cael@users.noreply.github.com>
2025-09-15 17:04:50 +0000

    feat: create summary_repository

---
+++ lib/data/repository/summary_repository.dart
@@ -0,0 +1,37 @@
+import 'dart:convert';
+
+import 'package:hidroly/domain/models/global_statistic.dart';
+import 'package:logger/logger.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class SummaryRepository {
+  var logger = Logger();
+
+  final _asyncPrefs = SharedPreferencesAsync();
+  final _globalStatisticsKey = "globalStatistics";
+
+  Future<bool> saveGlobalStatistic(GlobalStatistic globalStatistic) async {
+    try {
+      await _asyncPrefs.setString(_globalStatisticsKey, jsonEncode(globalStatistic));
+      return true;
+    } catch (e) {
+      logger.e('Failed to save global statistic: $e');
+      return false;
+    }
+  }
+  
+  Future<GlobalStatistic?> readGlobalStatistic() async {
+    try {
+      final globalStatisticString = await _asyncPrefs.getString(_globalStatisticsKey);
+      if(globalStatisticString == null) {
+        return null;
+      }
+
+      final globalStatisticsJson = jsonDecode(globalStatisticString);
+      return GlobalStatistic.fromJson(globalStatisticsJson);
+    } catch (e) {
+      logger.e('Failed to read global statistic: $e');
+      return null;
+    }
+  }
+}
