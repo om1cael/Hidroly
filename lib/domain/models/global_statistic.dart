785c1b34d9114705e25ba24ee8251a9c7e12ad18
om1cael <188113439+om1cael@users.noreply.github.com>
2025-09-15 18:17:03 +0000

    feat(global_statistic): add copyWith and toJson methods

--- lib/domain/models/global_statistic.dart
+++ lib/domain/models/global_statistic.dart
@@ -11,12 +11,30 @@ class GlobalStatistic {
     required this.averageIntake,
   });
 
-  factory GlobalStatistic.fromJson(Map<String, Object> json) {
+  factory GlobalStatistic.fromJson(Map<String, dynamic> json) {
     return GlobalStatistic(
       currentStreak: json['currentStreak'] as int,
       bestStreak: json['bestStreak'] as int,
       totalIntake: json['totalIntake'] as int,
       averageIntake: json['averageIntake'] as int,
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'currentStreak': currentStreak,
+      'bestStreak': bestStreak,
+      'totalIntake': totalIntake,
+      'averageIntake': averageIntake,
+    };
+  }
+
+  GlobalStatistic copyWith({int? currentStreak, int? bestStreak, int? totalIntake, int? averageIntake}) {
+    return GlobalStatistic(
+      currentStreak: currentStreak ?? this.currentStreak,
+      bestStreak: bestStreak ?? this.bestStreak,
+      totalIntake: totalIntake ?? this.totalIntake,
+      averageIntake: averageIntake ?? this.averageIntake,
     );
   }
 }
