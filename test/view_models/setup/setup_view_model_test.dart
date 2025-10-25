eeb76a5486c17d41729dd47f3811f29e2324d548
Micael <188113439+om1cael@users.noreply.github.com>
2025-10-25 16:09:25 -0300

    refactor: setup screen (#119)
    * refactor: move setup files to ui layer
    
    * refactor: move getDailyGoal logic to view model
    
    * refactor: move value notifiers and notification logic to view model
    
    * refactor: create dispose method on view model
    
    * refactor: rename setup steps class names to clarify what they do
    
    * test: setup setup view model test
    
    * test: implement daily goal calculation logic tests
    
    * refactor: introduce optional metric parameter on getDailyGoal method
    
    * test: implement imperial test to daily goal validation logic
    
    * refactor: make getDailyGoal receive string as age and weight parameter type
    
    * test: refactor setup_viewl_model test to use strings when calling getDailyGoal
    
    * test: improve weight variable names

---
+++ test/view_models/setup/setup_view_model_test.dart
@@ -0,0 +1,81 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:hidroly/provider/custom_cups_provider.dart';
+import 'package:hidroly/provider/day_provider.dart';
+import 'package:hidroly/provider/settings_provider.dart';
+import 'package:hidroly/ui/setup/view_models/setup_view_model.dart';
+import 'package:hidroly/utils/calculate_dailygoal.dart';
+import 'package:hidroly/utils/unit_tools.dart';
+import 'package:mocktail/mocktail.dart';
+
+class MockDayProvider extends Mock implements DayProvider {}
+class MockCustomCupsProvider extends Mock implements CustomCupsProvider {}
+class MockSettingsProvider extends Mock implements SettingsProvider {}
+
+void main() {
+  late SetupViewModel viewModel;
+  late MockDayProvider dayProvider;
+  late MockCustomCupsProvider customCupsProvider;
+  late MockSettingsProvider settingsProvider;
+
+  setUp(() {
+    dayProvider = MockDayProvider();
+    customCupsProvider = MockCustomCupsProvider();
+    settingsProvider = MockSettingsProvider();
+
+    viewModel = SetupViewModel(
+      dayProvider: dayProvider, 
+      customCupsProvider: customCupsProvider, 
+      settingsProvider: settingsProvider
+    );
+  });
+
+  group('Daily Goal Validation:', () {
+    test('Returns null if values are not valid', () {
+      final age = 'a';
+      final weight = 'b';
+
+      int? value = viewModel.getDailyGoal(age, weight);
+      expect(value, null);
+    });
+
+    test('Returns null if one of the values are not valid', () {
+      final age = '24';
+      final weight = 'b';
+
+      int? value = viewModel.getDailyGoal(age, weight);
+      expect(value, null);
+    });
+
+    test('Returns a valid daily goal if values are valid', () {
+      final age = '24';
+      final weight = '65';
+
+      int expectedDailyGoal = CalculateDailyGoal().calculate(
+        int.parse(age), 
+        int.parse(weight)
+      );
+
+      int? value = viewModel.getDailyGoal(age, weight);
+      expect(value, expectedDailyGoal);
+    });
+
+    test('Converts weight from lb to kg when metric system is disabled', () {
+      final age = '24';
+      final weightInLb = '145';
+
+      final weightInKg = UnitTools.lbToKg(int.parse(weightInLb));
+      final expectedDailyGoal = CalculateDailyGoal().calculate(
+        int.parse(age), 
+        weightInKg
+      );
+
+      final result = viewModel.getDailyGoal(
+        age, 
+        weightInLb,
+        providedMetricValue: false
+      );
+
+      expect(result, expectedDailyGoal);
+    });
+  });
+}
