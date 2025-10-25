3f2e8861d35f04110603577fa210698bfb307979
om1cael <188113439+om1cael@users.noreply.github.com>
2025-10-25 16:41:22 +0000

    test: implement daily goal calculation logic tests

--- test/view_models/setup/setup_view_model_test.dart
+++ test/view_models/setup/setup_view_model_test.dart
@@ -1,8 +1,10 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:hidroly/provider/custom_cups_provider.dart';
 import 'package:hidroly/provider/day_provider.dart';
 import 'package:hidroly/provider/settings_provider.dart';
 import 'package:hidroly/ui/setup/view_models/setup_view_model.dart';
+import 'package:hidroly/utils/calculate_dailygoal.dart';
 import 'package:mocktail/mocktail.dart';
 
 class MockDayProvider extends Mock implements DayProvider {}
@@ -25,5 +27,36 @@ void main() {
       customCupsProvider: customCupsProvider, 
       settingsProvider: settingsProvider
     );
+  });
+
+  group('Daily Goal Validation:', () {
+    test('Returns null if values are not valid', () {
+      final ageController = TextEditingController(text: null);
+      final weightController = TextEditingController(text: null);
+
+      int? value = viewModel.getDailyGoal(ageController, weightController);
+      expect(value, null);
+    });
+
+    test('Returns null if one of the values are not valid', () {
+      final ageController = TextEditingController(text: '24');
+      final weightController = TextEditingController(text: null);
+
+      int? value = viewModel.getDailyGoal(ageController, weightController);
+      expect(value, null);
+    });
+
+    test('Returns a valid daily goal if values are valid', () {
+      final ageController = TextEditingController(text: '24');
+      final weightController = TextEditingController(text: '65');
+
+      final age = int.parse(ageController.text);
+      final weight = int.parse(weightController.text);
+
+      int dailyGoal = CalculateDailyGoal().calculate(age, weight);
+
+      int? value = viewModel.getDailyGoal(ageController, weightController);
+      expect(value, dailyGoal);
+    });
   });
 }
