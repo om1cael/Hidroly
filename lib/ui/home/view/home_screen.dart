8641c7b53d2c02c28f8c08ba3690f0689a51a333
om1cael <188113439+om1cael@users.noreply.github.com>
2025-11-18 16:02:12 +0000

    refactor(home_screen): use futurebuilder to load data

--- lib/ui/home/view/home_screen.dart
+++ lib/ui/home/view/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:hidroly/l10n/app_localizations.dart';
 import 'package:hidroly/ui/home/view_models/home_view_model.dart';
 import 'package:hidroly/ui/summary/view/summary_screen.dart';
@@ -22,39 +23,55 @@ class _HomeScreenState extends State<HomeScreen> {
   
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: IndexedStack(
-        index: _currentIndex,
-        children: _screens,
-      ),
-      bottomNavigationBar: Theme(
-        data: Theme.of(context).copyWith(
-          splashColor: Colors.transparent,
-        ),
-        child: BottomNavigationBar(
-          items: <BottomNavigationBarItem>[
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              label: AppLocalizations.of(context)!.bottomNavHomeLabel,
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.bar_chart),
-              label: AppLocalizations.of(context)!.bottomNavSummaryLabel,
-            ),
-          ],
-          onTap: (newIndex) => setState(() {
-            if(newIndex == 1) {
-              // Force the SummaryScreen lifecycle to start over
-              // So we can get the last statistics on the initState method
-              _screens.removeAt(1);
-              _screens.insert(1, SummaryScreen(key: UniqueKey(),));
-            }
+    return FutureBuilder(
+      future: widget.viewModel.initializeData(),
+      builder: (context, asyncSnapshot) {
+        if(asyncSnapshot.connectionState == ConnectionState.waiting) {
+          return Container(
+            color: Theme.of(context).colorScheme.onSurface,
+            child: Center(child: CircularProgressIndicator(),),
+          );
+        } else if(asyncSnapshot.hasData && asyncSnapshot.data == false) {
+          WidgetsBinding.instance.addPostFrameCallback((_) {
+            context.go('/setup');
+          });
+        }
 
-            _currentIndex = newIndex;
-          }),
-          currentIndex: _currentIndex,
-        ),
-      ),
+        return Scaffold(
+          body: IndexedStack(
+            index: _currentIndex,
+            children: _screens,
+          ),
+          bottomNavigationBar: Theme(
+            data: Theme.of(context).copyWith(
+              splashColor: Colors.transparent,
+            ),
+            child: BottomNavigationBar(
+              items: <BottomNavigationBarItem>[
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.home),
+                  label: AppLocalizations.of(context)!.bottomNavHomeLabel,
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.bar_chart),
+                  label: AppLocalizations.of(context)!.bottomNavSummaryLabel,
+                ),
+              ],
+              onTap: (newIndex) => setState(() {
+                if(newIndex == 1) {
+                  // Force the SummaryScreen lifecycle to start over
+                  // So we can get the last statistics on the initState method
+                  _screens.removeAt(1);
+                  _screens.insert(1, SummaryScreen(key: UniqueKey(),));
+                }
+        
+                _currentIndex = newIndex;
+              }),
+              currentIndex: _currentIndex,
+            ),
+          ),
+        );
+      }
     );
   }
 }
