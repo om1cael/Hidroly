d0850573915abecbf39bfcb625c84377bd191ac9
om1cael <188113439+om1cael@users.noreply.github.com>
2026-02-15 19:00:20 -0300

    feat: add header

--- lib/features/setup/ui/setup_view.dart
+++ lib/features/setup/ui/setup_view.dart
@@ -9,16 +9,32 @@ class SetupView extends StatelessWidget {
       body: SafeArea(
         minimum: EdgeInsets.all(48.0),
         child: Column(
+          spacing: 64,
           mainAxisAlignment: .center,
           crossAxisAlignment: .center,
           children: [
+            Column(
+              children: [
+                Text(
+                  'Welcome',
+                  style: Theme.of(context).textTheme.displayLarge,
+                ),
+                Text(
+                  'Let\'s set up your profile to personalize your daily water goal.',
+                  style: Theme.of(context).textTheme.bodyMedium,
+                  textAlign: .center,
+                ),
+              ],
+            ),
+
             Form(
               child: Column(
-                spacing: 16,
+                spacing: 24,
                 children: [
                   TextFormField(
                     decoration: InputDecoration(
                       label: Text('Age'),
+                      counterText: "",
                       border: OutlineInputBorder(
                         borderRadius: BorderRadius.circular(24),
                       ),
@@ -30,6 +46,7 @@ class SetupView extends StatelessWidget {
                   TextFormField(
                     decoration: InputDecoration(
                       label: Text('Weight'),
+                      counterText: "",
                       border: OutlineInputBorder(
                         borderRadius: BorderRadius.circular(24),
                       ),
