0951aa39628350aa0807a025b5bb22b0bbc89cd6
om1cael <188113439+om1cael@users.noreply.github.com>
2026-02-15 19:45:21 -0300

    feat: add scroll

--- lib/features/setup/ui/setup_view.dart
+++ lib/features/setup/ui/setup_view.dart
@@ -11,50 +11,61 @@ class SetupView extends StatelessWidget {
     return Scaffold(
       body: SafeArea(
         minimum: EdgeInsets.all(48.0),
-        child: Column(
-          spacing: 64,
-          mainAxisAlignment: .center,
-          crossAxisAlignment: .center,
-          children: [
-            HeaderText(
-              title: 'Welcome',
-              subtitle: 'Let\'s set up your profile to personalize your daily water goal.',
-            ),
-
-            Form(
+        child: CustomScrollView(
+          physics: ClampingScrollPhysics(),
+          slivers: [
+            SliverFillRemaining(
+              hasScrollBody: false,
               child: Column(
-                spacing: 24,
+                spacing: 64,
+                crossAxisAlignment: .center,
                 children: [
-                  NumberInputFormField(
-                    label: 'Age',
-                    maxLength: 3,
-                  ),
-                  NumberInputFormField(
-                    label: 'Weight',
-                    suffix: 'kg',
-                    maxLength: 3,
-                  ),
-                  SegmentedButton(
-                    segments: [
-                      ButtonSegment(
-                        label: Text('Metric'),
-                        value: UnitSystem.metric
-                      ),
-                      ButtonSegment(
-                        label: Text('Imperial'),
-                        value: UnitSystem.imperial
-                      ),
-                    ], 
-                    selected: {UnitSystem.metric},
-                    onSelectionChanged: (newSelection) {
-                      // TODO: Change selected segment
-                    },
-                  ),
-                ],
-              )
-            ),
+                  const Spacer(),
 
-            Text('Your data never leaves your device.')
+                  HeaderText(
+                    title: 'Welcome',
+                    subtitle: 'Let\'s set up your profile to personalize your daily water goal.',
+                  ),
+
+                  Form(
+                    child: Column(
+                      spacing: 24,
+                      children: [
+                        NumberInputFormField(
+                          label: 'Age',
+                          maxLength: 3,
+                        ),
+                        NumberInputFormField(
+                          label: 'Weight',
+                          suffix: 'kg',
+                          maxLength: 3,
+                        ),
+                        SegmentedButton(
+                          segments: [
+                            ButtonSegment(
+                              label: Text('Metric'),
+                              value: UnitSystem.metric
+                            ),
+                            ButtonSegment(
+                              label: Text('Imperial'),
+                              value: UnitSystem.imperial
+                            ),
+                          ], 
+                          selected: {UnitSystem.metric},
+                          onSelectionChanged: (newSelection) {
+                            // TODO: Change selected segment
+                          },
+                        ),
+                      ],
+                    )
+                  ),
+
+                  Text('Your data never leaves your device.'),
+
+                  const Spacer(),
+                ],
+              ),
+            )
           ],
         )
       ),
