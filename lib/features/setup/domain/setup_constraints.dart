d9e6d733eab721af42639afc5c1afc5687fcba36
om1cael <188113439+om1cael@users.noreply.github.com>
2026-02-16 16:54:50 -0300

    feat: set max water suggestion

--- lib/features/setup/domain/setup_constraints.dart
+++ lib/features/setup/domain/setup_constraints.dart
@@ -3,8 +3,11 @@ abstract final class SetupConstraints {
   static const int maxAge = 100;
 
   static const int minWeightKg = 35;
-  static const int maxWeightKg = 350;
+  static const int maxWeightKg = 150;
 
   static int get minWeightLb => (minWeightKg * 2.205).round();
   static int get maxWeightLb => (maxWeightKg * 2.205).round();
+
+  static const int maxWaterSuggestionMl = 4000;
+  static int get maxWaterSuggestionOz => (maxWaterSuggestionMl / 29.574).round();
 }
