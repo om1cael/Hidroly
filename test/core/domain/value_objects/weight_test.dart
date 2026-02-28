f1acafd19c9d437bfed1b35741a96b9ac0515315
om1cael <188113439+om1cael@users.noreply.github.com>
2026-02-28 18:17:16 -0300

    test: add extra tests for the weight value object

--- test/core/domain/value_objects/weight_test.dart
+++ test/core/domain/value_objects/weight_test.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:hidroly/core/domain/enums/unit_systems.dart';
 import 'package:hidroly/core/domain/exceptions/invalid_input_exception.dart';
 import 'package:hidroly/core/domain/value_objects/weight.dart';
 
@@ -6,6 +7,9 @@ void main() {
   group('Weight Value Object Testing', () {
     final minWeight = Weight.minWeight;
     final maxWeight = Weight.maxWeight;
+
+    final minWeightLb = Weight.minWeightLb;
+    final maxWeightLb = Weight.maxWeightLb;
 
     test('Accepts withing range', () {
       expect(Weight.kg(minWeight), isA<Weight>());
@@ -22,6 +26,22 @@ void main() {
       final expectedKg = 65;
 
       expect(Weight.fromLb(lbWeight).kg, equals(expectedKg));
+    });
+
+    test('Rejects out of range lb conversion', () {
+      // This is needed because the code rounds the conversion.
+      final testFailureRange = 5;
+
+      expect(() => Weight.fromLb(minWeightLb - testFailureRange), throwsA(isA<InvalidInputException>()));
+      expect(() => Weight.fromLb(maxWeightLb + testFailureRange), throwsA(isA<InvalidInputException>()));
+    });
+
+    test('Gets correct value based on the unit system', () {
+      expect(Weight.minWeightFor(UnitSystem.metric), equals(minWeight));
+      expect(Weight.maxWeightFor(UnitSystem.metric), equals(maxWeight));
+
+      expect(Weight.minWeightFor(UnitSystem.imperial), equals(minWeightLb));
+      expect(Weight.maxWeightFor(UnitSystem.imperial), equals(maxWeightLb));
     });
   });
 }
