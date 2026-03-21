46e89fd9da3df14fae549ca07302d441d1812f26
om1cael <188113439+om1cael@users.noreply.github.com>
2026-03-21 14:15:50 -0300

    feat: extract hydration progress indicator widget

--- lib/features/hydration/ui/view/hydration_view.dart
+++ lib/features/hydration/ui/view/hydration_view.dart
@@ -2,6 +2,7 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:hidroly/core/domain/enums/unit_systems.dart';
+import 'package:hidroly/features/hydration/ui/view/components/hydration_progress_indicator.dart';
 import 'package:hidroly/features/hydration/ui/view_model/hydration_view_model.dart';
 
 class HydrationView extends ConsumerWidget {
@@ -20,31 +21,10 @@ class HydrationView extends ConsumerWidget {
             children: [
               state.when(
                 data: (data) {
-                  return SizedBox(
-                    height: 280,
-                    width: 280,
-                    child: Stack(
-                      alignment: .xy(0, 0),
-                      children: [
-                        SizedBox.expand(
-                          child: CircularProgressIndicator(
-                            value: (data.day.currentAmount / data.day.dailyGoal).clamp(0, 1),
-                            strokeWidth: 16,
-                            strokeCap: .round,
-                            backgroundColor: Theme.of(context).colorScheme.surfaceContainer,
-                            color: Theme.of(context).colorScheme.primary,
-                          ),
-                        ),
-                        Column(
-                          mainAxisAlignment: .center,
-                          crossAxisAlignment: .center,
-                          children: [
-                            Text('${data.day.currentAmount} ${getUnitText(data.unitSystem)}', style: Theme.of(context).textTheme.headlineLarge),
-                            Text('${'of'.tr()} ${data.day.dailyGoal} ${getUnitText(data.unitSystem)}', style: Theme.of(context).textTheme.bodyLarge,),
-                          ],
-                        )
-                      ],
-                    ),
+                  return HydrationProgressIndicator(
+                    currentAmount: data.day.currentAmount, 
+                    dailyGoal: data.day.dailyGoal, 
+                    unitText: getUnitText(data.unitSystem),
                   );
                 }, 
                 error: (_, _) => Placeholder(), 
