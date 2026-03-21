d615575e3a81a4572c62ee745d325a0e4fa8d8f6
om1cael <188113439+om1cael@users.noreply.github.com>
2026-03-21 13:15:38 -0300

    feat: add hydration circle and text

--- lib/features/hydration/view/hydration_view.dart
+++ lib/features/hydration/view/hydration_view.dart
@@ -5,6 +5,43 @@ class HydrationView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: .center,
+            crossAxisAlignment: .center,
+            children: [
+              SizedBox(
+                height: 280,
+                width: 280,
+                child: Stack(
+                  alignment: .xy(0, 0),
+                  children: [
+                    SizedBox.expand(
+                      child: CircularProgressIndicator(
+                        value: .5,
+                        strokeWidth: 16,
+                        strokeCap: .round,
+                        backgroundColor: Theme.of(context).colorScheme.surfaceContainer,
+                        color: Theme.of(context).colorScheme.primary,
+                      ),
+                    ),
+                    Column(
+                      mainAxisAlignment: .center,
+                      crossAxisAlignment: .center,
+                      children: [
+                        Text('1200ml', style: Theme.of(context).textTheme.headlineLarge),
+                        Text('of 2000ml', style: Theme.of(context).textTheme.bodyLarge,),
+                      ],
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+        )
+      ),
+    );
   }
 }
