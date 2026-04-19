4a29a25bc376e643a3c7d6d0fc11f385030ca797
om1cael <188113439+om1cael@users.noreply.github.com>
2026-04-19 11:36:47 -0300

    feat: implement error screen

--- lib/features/migration/ui/view/migration_view.dart
+++ lib/features/migration/ui/view/migration_view.dart
@@ -154,14 +154,108 @@ class MigrationView extends ConsumerWidget {
 
                 FilledButton(
                   onPressed: () {
-                    // save flag?
                     context.go('/');
                   }, 
                   child: Text('Let\'s go!')
                 ),
               ],
             ),
-            error: (error) => Text(error),
+            error: (error) => Column(
+              spacing: 32,
+              mainAxisAlignment: .center,
+              crossAxisAlignment: .center,
+              children: [            
+                Column(
+                  children: [
+                    CircleAvatar(
+                      radius: 48,
+                      backgroundColor: Colors.red,
+                      child: Icon(Icons.priority_high, size: 64,)
+                    ),
+            
+                    SizedBox(height: 8,),
+            
+                    Text('Sorry', textAlign: .center, style: Theme.of(context).textTheme.headlineLarge,),
+                    Text(
+                      'An error happened during the migration. Feel free to report the error below on the project\'s repository.',
+                      textAlign: .center,
+                      style: Theme.of(context).textTheme.bodyMedium,
+                    ),
+                  ],
+                ),
+
+                Container(
+                  height: 100,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(16),
+                    color: Theme.of(context).colorScheme.surfaceContainer
+                  ),
+                  child: SingleChildScrollView(
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Text(error),
+                    ),
+                  ),
+                ),
+                
+                Column(
+                  mainAxisAlignment: .center,
+                  crossAxisAlignment: .stretch,
+                  children: [
+                    FilledButton(
+                      onPressed: () {
+                        showDialog(
+                          context: context, 
+                          builder: (_) {
+                            return AlertDialog(
+                              title: Text('Are you sure?'),
+                              content: Text('This isn\'t guaranteed to work. If it doesn\'t work again, you can report the error and wait for a fix. Your data will not be lost.'),
+                              actions: [
+                                TextButton(onPressed: () => Navigator.pop(context), child: Text('No')),
+                                FilledButton(
+                                  onPressed: () {
+                                    Navigator.pop(context);
+                                    ref.read(migrationViewModelProvider.notifier).startMigration();
+                                  }, 
+                                  child: Text('Proceed with it')),
+                              ],
+                            );
+                          }
+                        );
+                      }, 
+                      child: Text('Try again')
+                    ),
+                    TextButton(
+                      onPressed: () async {
+                        showDialog(
+                          context: context, 
+                          builder: (_) {
+                            return AlertDialog(
+                              title: Text('Are you sure?'),
+                              content: Text('You will not be able to migrate your data in the future.'),
+                              actions: [
+                                TextButton(onPressed: () => Navigator.pop(context), child: Text('No')),
+                                FilledButton(
+                                  onPressed: () async {
+                                    Navigator.pop(context);
+                                    ref.read(migrationViewModelProvider.notifier).ignoreMigration();
+
+                                    if(!context.mounted) return;
+                                    context.go('/setup');
+                                  }, 
+                                  child: Text('Proceed with it')),
+                              ],
+                            );
+                          }
+                        );
+                      },
+                      child: Text('I don\'t want to migrate anymore')
+                    ),
+                  ],
+                )
+              ]
+            )
           ),
         )
       ),
