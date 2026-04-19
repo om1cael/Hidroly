7d212f5c367bd5773e9f4ee4318fa25bed0fe0c6
om1cael <188113439+om1cael@users.noreply.github.com>
2026-04-19 08:35:16 -0300

    feat(migration_state): add done state

--- lib/features/migration/ui/state/migration_state.dart
+++ lib/features/migration/ui/state/migration_state.dart
@@ -8,5 +8,7 @@ abstract class MigrationState with _$MigrationState {
   
   factory MigrationState.processing({required double progress}) = _Processing;
 
+  factory MigrationState.done() = _Done;
+
   factory MigrationState.error({required String error}) = _Error;
 }
