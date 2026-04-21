6788466a49718baa2cf7b36169301da97fd29df5
om1cael <188113439+om1cael@users.noreply.github.com>
2026-04-21 13:35:38 -0300

    fix: use async value on view

--- lib/core/ui/view/notification_settings_card.dart
+++ lib/core/ui/view/notification_settings_card.dart
@@ -12,87 +12,95 @@ class NotificationSettingsCard extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final state = ref.watch(notificationSettingsCardViewModelProvider);
 
-    return Card(
-      child: Column(
-        children: [
-          ListTile(
-            title: Text('I wake up at'),
-            subtitle: Text(DateFormat.jm().format(DateTime(2026, 4, 21, state.wakeUpTime.hour, state.wakeUpTime.minute))),
-            leading: CircleAvatar(child: Icon(Icons.wb_sunny)),
-            trailing: Icon(Icons.chevron_right),
-            onTap: () async {
-              final wakeUpTime = await showTimePicker(
-                context: context, 
-                initialTime: state.wakeUpTime,
-              );
+    return state.when(
+      error: (_, _) => Text('errorOccurred'.tr()), 
+      loading: () => Center(child: CircularProgressIndicator(),),
+      data: (data) => Card(
+        child: Column(
+          children: [
+            ListTile(
+              title: Text('I wake up at'),
+              subtitle: Text(DateFormat.jm().format(DateTime(2026, 4, 21, data.wakeUpTime.hour, data.wakeUpTime.minute))),
+              leading: CircleAvatar(child: Icon(Icons.wb_sunny)),
+              trailing: Icon(Icons.chevron_right),
+              onTap: () async {
+                final wakeUpTime = await showTimePicker(
+                  context: context, 
+                  initialTime: data.wakeUpTime,
+                );
 
-              if(wakeUpTime == null) return;
+                if(wakeUpTime == null) return;
 
-              ref
-                .read(notificationSettingsCardViewModelProvider.notifier)
-                .setWakeUpTime(wakeUpTime);
-            },
-          ),
-          ListTile(
-            title: Text('I go to sleep at'),
-            subtitle: Text(DateFormat.jm().format(DateTime(2026, 4, 21, state.sleepTime.hour, state.sleepTime.minute))),
-            leading: CircleAvatar(child: Icon(Icons.bedtime),),
-            trailing: Icon(Icons.chevron_right),
-            onTap: () async {
-              final sleepTime = await showTimePicker(
-                context: context, 
-                initialTime: state.sleepTime,
-              );
+                ref
+                  .read(notificationSettingsCardViewModelProvider.notifier)
+                  .setWakeUpTime(wakeUpTime);
+              },
+            ),
+            ListTile(
+              title: Text('I go to sleep at'),
+              subtitle: Text(DateFormat.jm().format(DateTime(2026, 4, 21, data.sleepTime.hour, data.sleepTime.minute))),
+              leading: CircleAvatar(child: Icon(Icons.bedtime),),
+              trailing: Icon(Icons.chevron_right),
+              onTap: () async {
+                final sleepTime = await showTimePicker(
+                  context: context, 
+                  initialTime: data.sleepTime,
+                );
 
-              if(sleepTime == null) return;
+                if(sleepTime == null) return;
 
-              ref
-                .read(notificationSettingsCardViewModelProvider.notifier)
-                .setSleepTime(sleepTime);
-            },
-          ),
-          ListTile(
-            title: Text('Remind me each'),
-            subtitle: Text('${state.notificationFrequency} hours'),
-            leading: CircleAvatar(child: Icon(Icons.repeat),),
-            trailing: Icon(Icons.chevron_right),
-            onTap: () {
-              showModalBottomSheet(
-                context: context, 
-                builder: (context) {
-                  return Consumer(
-                    builder: (_, ref, _) {
-                      final state = ref.watch(notificationSettingsCardViewModelProvider);
-                      
-                      return SafeArea(
-                        child: RadioGroup<int>(
-                          groupValue: state.notificationFrequency,
-                          onChanged: (value) {
-                            if(value == null) return;
+                ref
+                  .read(notificationSettingsCardViewModelProvider.notifier)
+                  .setSleepTime(sleepTime);
+              },
+            ),
+            ListTile(
+              title: Text('Remind me each'),
+              subtitle: Text('${data.notificationFrequency} hours'),
+              leading: CircleAvatar(child: Icon(Icons.repeat),),
+              trailing: Icon(Icons.chevron_right),
+              onTap: () {
+                showModalBottomSheet(
+                  context: context, 
+                  builder: (context) {
+                    return Consumer(
+                      builder: (_, ref, _) {
+                        final state = ref.watch(notificationSettingsCardViewModelProvider);
+                        
+                        return state.when(
+                          error: (_, _) => Text('errorOccurred'.tr()), 
+                          loading: () => Center(child: CircularProgressIndicator(),),
+                          data: (data) => SafeArea(
+                            child: RadioGroup<int>(
+                              groupValue: data.notificationFrequency,
+                              onChanged: (value) {
+                                if(value == null) return;
 
-                            ref
-                              .read(notificationSettingsCardViewModelProvider.notifier)
-                              .setNotificationFrequency(value);
-                          }, 
-                          child: Column(
-                            mainAxisSize: .min,
-                            children: [
-                              for(final frequency in availableFrequencies)
-                                ListTile(
-                                  title: Text('$frequency hours'),
-                                  leading: Radio<int>(value: frequency,)
-                                ),
-                            ],
-                          )
-                        ),
-                      );
-                    }
-                  );
-                }
-              );
-            },
-          ),
-        ],
+                                ref
+                                  .read(notificationSettingsCardViewModelProvider.notifier)
+                                  .setNotificationFrequency(value);
+                              }, 
+                              child: Column(
+                                mainAxisSize: .min,
+                                children: [
+                                  for(final frequency in availableFrequencies)
+                                    ListTile(
+                                      title: Text('$frequency hours'),
+                                      leading: Radio<int>(value: frequency,)
+                                    ),
+                                ],
+                              )
+                            ),
+                          ), 
+                        );
+                      }
+                    );
+                  }
+                );
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
