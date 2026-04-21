598ca65996d733bbb5e783e53fd40c93f469d7e5
om1cael <188113439+om1cael@users.noreply.github.com>
2026-04-21 08:22:33 -0300

    feat: implement dumb notification

--- lib/infra/notifications/notification_service.dart
+++ lib/infra/notifications/notification_service.dart
@@ -1,10 +1,10 @@
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 class NotificationService {
-  Future<void> initialize() async {
-    final flutterLocalNotificationsPlugin =
-      FlutterLocalNotificationsPlugin();
-    
+  final flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
+
+  Future<void> initialize() async {    
     // TODO: Change icon
     const androidInitializationSettings = 
       AndroidInitializationSettings('@mipmap/ic_launcher');
@@ -15,6 +15,27 @@ class NotificationService {
     await flutterLocalNotificationsPlugin.initialize(
       settings: initializationSettings,
       onDidReceiveBackgroundNotificationResponse: notificationActionTapResponse,
+    );
+  }
+
+  Future<void> showNotification() async {
+    final androidNotificationDetails =
+      AndroidNotificationDetails(
+        'h_reminder', 
+        'Reminders',
+        channelDescription: 'Receive reminders to drink water',
+        importance: .high,
+        priority: .high,
+      );
+    
+    final notificationDetails =
+      NotificationDetails(android: androidNotificationDetails);
+    
+    await flutterLocalNotificationsPlugin.show(
+      id: 0,
+      title: 'Drink water',
+      body: 'It is time to drink water',
+      notificationDetails: notificationDetails,
     );
   }
 
