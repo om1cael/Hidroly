20c5e0a35f5c9159a92519609fb012dfdeba6f23
om1cael <188113439+om1cael@users.noreply.github.com>
2026-04-25 13:14:19 -0300

    build(deps): remove flutter local notifications

--- lib/infra/notifications/local_notification_service.dart
+++ lib/infra/notifications/local_notification_service.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:hidroly/core/data/repositories/day_repository_impl.dart';
 import 'package:hidroly/core/data/repositories/settings_repository_impl.dart';
 import 'package:hidroly/core/domain/enums/unit_systems.dart';
