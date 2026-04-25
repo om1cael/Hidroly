63190504c3c779ae3b2dd16ff3fc6d2a64c0c673
Micael <188113439+om1cael@users.noreply.github.com>
2026-04-25 12:34:04 -0300

    ci: add continuous distribution workflow (#172)
    * build: change build.gradle.kts to accept github secrets
    
    * cd: create d_man
    
    * cd: fix flutter setup
    
    * ci: fix release step
    
    * ci: set java version to 17
    
    * build: fix build.gradle.kts
    
    * ci: fix release and split per abi
    
    * ci: remove token
    
    * ci: generate release notes
    
    * ci: fix generate release notes

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,11 +11,33 @@ plugins {
 
 val keystoreProperties = Properties()
 val keystorePropertiesFile = rootProject.file("key.properties")
-val hasKeystore = keystorePropertiesFile.exists()
 
-if (hasKeystore) {
+val keyAliasVar = if (keystorePropertiesFile.exists()) {
     keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+    keystoreProperties["keyAlias"] as String
+} else {
+    System.getenv("KEY_ALIAS") ?: ""
 }
+
+val keyPasswordVar = if (keystorePropertiesFile.exists()) {
+    keystoreProperties["keyPassword"] as String
+} else {
+    System.getenv("KEY_PASSWORD") ?: ""
+}
+
+val storePasswordVar = if (keystorePropertiesFile.exists()) {
+    keystoreProperties["storePassword"] as String
+} else {
+    System.getenv("KEYSTORE_PASSWORD") ?: ""
+}
+
+val storeFileVar = if (keystorePropertiesFile.exists()) {
+    keystoreProperties["storeFile"]?.let { file(it) }
+} else {
+    file("upload-keystore.jks") 
+}
+
+val canSign = keyAliasVar.isNotEmpty() && keyPasswordVar.isNotEmpty()
 
 android {
     namespace = "com.om1cael.hidroly"
@@ -34,7 +56,7 @@ android {
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_17.toString()
+        jvmTarget = "17"
     }
 
     defaultConfig {
@@ -48,20 +70,20 @@ android {
         versionName = flutter.versionName
     }
 
-    if (hasKeystore) {
-        signingConfigs {
+    signingConfigs {
+        if (canSign) {
             create("release") {
-                keyAlias = keystoreProperties["keyAlias"] as String
-                keyPassword = keystoreProperties["keyPassword"] as String
-                storeFile = keystoreProperties["storeFile"]?.let { file(it) }
-                storePassword = keystoreProperties["storePassword"] as String
+                keyAlias = keyAliasVar
+                keyPassword = keyPasswordVar
+                storeFile = storeFileVar
+                storePassword = storePasswordVar
             }
         }
     }
 
     buildTypes {
         release {
-            if (hasKeystore) {
+            if (canSign) {
                 signingConfig = signingConfigs.getByName("release")
             }
         }
