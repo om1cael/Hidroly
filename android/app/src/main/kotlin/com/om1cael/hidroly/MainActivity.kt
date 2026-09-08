ea37e2f1eed3e809f1a7a55744fb84f9134aca65
om1cael <188113439+om1cael@users.noreply.github.com>
2026-09-08 11:59:19 -0300

    refactor: import flutter fragment activity

--- android/app/src/main/kotlin/com/om1cael/hidroly/MainActivity.kt
+++ android/app/src/main/kotlin/com/om1cael/hidroly/MainActivity.kt
@@ -1,5 +1,5 @@
 package com.om1cael.hidroly
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
 class MainActivity : FlutterFragmentActivity()
