2588129a6ad557f55b05f1599bcb4a00cfe02c66
om1cael <188113439+om1cael@users.noreply.github.com>
2026-09-12 12:46:53 -0300

    refactor: remove unused import

--- lib/features/settings/ui/view/components/personal_goal_sheet.dart
+++ lib/features/settings/ui/view/components/personal_goal_sheet.dart
@@ -1,7 +1,6 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:hidroly/core/domain/hydration_constraints.dart';
 import 'package:hidroly/core/domain/value_objects/goal.dart';
 import 'package:hidroly/core/ui/components/number_input_form_field.dart';
 import 'package:hidroly/core/ui/extensions/snack_bar_extension.dart';
