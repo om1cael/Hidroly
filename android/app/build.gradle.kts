01a45165d9993b2a0c6abcc11ae91804dbb47b2f
Micael <188113439+om1cael@users.noreply.github.com>
2026-09-12 13:47:31 -0300

    fix version code (#218)
    * build: set base version code for default release
    
    * ci: also use ignore abi flag on default apk build

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -121,14 +121,22 @@ androidComponents {
             val abi = output.filters
                 .find { it.filterType == ABI }
                 ?.identifier
-
+            
+            val flavor = variant.productFlavors
+                .firstOrNull()
+                ?.second
+            
             val abiVersionCode = abiCodes[abi]
 
             if (abiVersionCode != null) {
                 val baseVersionCode = output.versionCode.get()
-                output.versionCode.set(
+                val versionCode = if (flavor == "fdroid") {
                     baseVersionCode * 10 + abiVersionCode
-                )
+                } else {
+                    3000 + (baseVersionCode * 10 + abiVersionCode)
+                }
+
+                output.versionCode.set(versionCode)
             }
         }
     }
